3db2a5a89dd1af195e138994f68e4dc427937ccd
peter carandang <peter.f.a.carandang@accenture.com>
2024-08-13 03:22:43 +0000

    Fragment Changes

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -2,14 +2,24 @@ sap.ui.define([
   "sap/ui/core/mvc/Controller",
   "sap/ui/core/UIComponent",	
   "sap/ui/core/routing/History",
+  "sap/ui/model/json/JSONModel"
 ],
-function (Controller, UIComponent, History) {
+function (Controller, UIComponent, History, JSONModel) {
   "use strict";
 
   return Controller.extend("app.controller.Detail", {
       onInit: function () {
         const oRouter = UIComponent.getRouterFor(this);
         oRouter.getRoute("RouteDetail").attachPatternMatched(this._onObjectMatched, this);
+
+        this.getView().setModel(new JSONModel({
+            Value1: null,
+            Value2: null,
+            ValueState1: "None",
+            ValueState2: "None",
+            editable: false,
+            required: false,
+        }), "ViewModel");
       },
 
       //When triggered, puts passed parameters in Input fields
@@ -17,11 +27,15 @@ function (Controller, UIComponent, History) {
       _onObjectMatched: function (oEvent) { 
         const sParam1 = oEvent.getParameter("arguments").Param1;
         const sParam2 = oEvent.getParameter("arguments").Param2;
-        const oText1 = this.getView().byId("idInput1");        
+        /*const oText1 = this.getView().byId("idInput1");
         const oText2 = this.getView().byId("idInput2");
               
         oText1.setValue(sParam1);
-        oText2.setValue(sParam2);
+        oText2.setValue(sParam2);*/
+
+        const oModel = this.getView().getModel("ViewModel");
+        oModel.setProperty("/Value1", sParam1);
+        oModel.setProperty("/Value2", sParam2);
       },
 
       //When triggered, when back button is pressed.
@@ -36,7 +50,7 @@ function (Controller, UIComponent, History) {
         } else {
           const oRouter = UIComponent.getRouterFor(this); 
           oRouter.navTo("RouteNavigation");
-        } 
+        }
       }
 
   });
--- webapp/controller/Navigation.controller.js
+++ webapp/controller/Navigation.controller.js
@@ -1,14 +1,22 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/core/UIComponent",	
-    "sap/m/MessageBox"
+    "sap/m/MessageBox",
+    "sap/ui/model/json/JSONModel"
 ],
-function (Controller, UIComponent, MessageBox) {
+function (Controller, UIComponent, MessageBox, JSONModel) {
     "use strict";
 
     return Controller.extend("app.controller.Navigation", {
         onInit: function () {
-
+            this.getView().setModel(new JSONModel({
+                Value1: null,
+                Value2: null,
+                ValueState1: "None",
+                ValueState2: "None",
+                editable: true,
+                required: true,
+            }), "ViewModel");
         },
 
         //Triggered whenever an Input has been changed
@@ -28,38 +36,43 @@ function (Controller, UIComponent, MessageBox) {
         onPress: function () {
             const oView = this.getView(),                
                 oResourceBundle = oView.getModel("i18n").getResourceBundle(),
-                oInput1 = oView.byId("idInput1"),
-                oInput2 = oView.byId("idInput2");
+                /*oInput1 = oView.byId("idInput1"),
+                oInput2 = oView.byId("idInput2")*/
+                oValue1 = oView.getModel("ViewModel").getProperty("/Value1"),
+                oValue2 = oView.getModel("ViewModel").getProperty("/Value2"),
+                oValueState1 = oView.getModel("ViewModel").getProperty("/ValueState1"),
+                oValueState2 = oView.getModel("ViewModel").getProperty("/ValueState2");
                 
             //Check if Input fields have values                
-            this.fnCheckFields(oInput1, oInput2);
+            this.fnCheckFields(oValue1, oValue2);
                 
             //Send error message if at least one of the input fields have error. Navigate if none have errors.                 
-            if (oInput1.getValueState() === "Error" || oInput2.getValueState() === "Error") {                
+            if (oValueState1 === "Error" || oValueState2 === "Error") {                
                 MessageBox.error(oResourceBundle.getText("ErrorMessage"));
             } else {                
-                this.fnNavigateToDetailPage(oInput1.getValue(), oInput2.getValue());                
+                this.fnNavigateToDetailPage(oValue1, oValue2);                
             }                
         },
 
         //Checks if values in Input fields are valid
         //oInput1 - Input object
         //oInput2 - 2nd Input Object
-        fnCheckFields: function(oInput1, oInput2) {
-            const sData1 = oInput1.getValue(),                
-                sData2 = oInput2.getValue();
+        fnCheckFields: function(oValue1, oValue2) {
+            const oView = this.getView();
+            /*const sData1 = oInput1.getValue(),                
+                sData2 = oInput2.getValue();*/
                 
             //Check if Input fields have values 
-            if (!sData1) { 
-                oInput1.setValueState("Error");
+            if (!oValue1) { 
+                oView.getModel("ViewModel").setProperty("/ValueState1", "Error");
             } else {                    
-                oInput1.setValueState("None");
+                oView.getModel("ViewModel").setProperty("/ValueState1", "None");
             }
 
-            if (!sData2) { 
-                oInput2.setValueState("Error");
+            if (!oValue2) { 
+                oView.getModel("ViewModel").setProperty("/ValueState2", "Error");
             } else {
-                oInput2.setValueState("None");
+                oView.getModel("ViewModel").setProperty("/ValueState2", "None");
             }                
         },
 
@@ -73,6 +86,38 @@ function (Controller, UIComponent, MessageBox) {
                 Param1: sData1, 
                 Param2: sData2                
             });                
+        },
+
+        onOpenDialog: function () {
+            if (!this.oDialog) {
+                this.oDialog = this.loadFragment({
+                    name: "app.fragment.Dialog"
+                });
+            }
+            this.oDialog.then(function (oDialog){ 
+                oDialog.open();
+
+                const oModel = this.getView().getModel("ViewModel");
+                oModel.setProperty("/Value3", null);
+            }.bind(this));
+        },
+
+        onClose: function () {
+            this.getView().byId("idDialog").close();
+        },
+
+        onPressGetSum: function() {
+            const oModel = this.getView().getModel("ViewModel"),
+                oResourceBundle = this.getView().getModel("i18n").getResourceBundle(),
+                oValue1 = oModel.getProperty("/Value1"),
+                oValue2 = oModel.getProperty("/Value2");
+
+            if (isNaN(oValue1) ||  isNaN(oValue2)) {
+                MessageBox.error(oResourceBundle.getText("calcError"));
+            } else {
+                oModel.setProperty("/Value3", Number(oValue1) + Number(oValue2));
+            }
+            
         }
     });
 });
